a688393380a32d1fe3f04023ea1a8029c40a5f64
Mary Farner <maryfarner@gmail.com>
2019-08-27 01:30:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,9 +16,12 @@ end
 
 def get_japanese_emoticon(file, emoti)
   hash = load_library(file)
-  hash[get_emoticon][emoti]
+  {hash[get_emoticon][emoti]? hash[get_emoticon][emoti] : "Sorry, that emoticon isn't recognized!"} 
+   
+  
 end
 
 def get_english_meaning(emoti)
-  # code goes here
+  hash = load_library(file)
+  hash[get_emoticon][emoti]
 end
